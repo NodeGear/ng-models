6bc9312711da6c9047547aee47a2966f4b93fd77
Matej Kramny <matejkramny@gmail.com>
2014-04-10 20:33:53 +0100

    Add models to the module

---
+++ bin/ng-models.js
@@ -0,0 +1,19 @@
+module.exports = {};
+
+var manifest = require('../lib/manifest.json').models;
+
+var models = require('../lib/ng-models');
+module.exports.init = function() {
+	models.init.apply(this, arguments);
+
+	loadManifest();
+
+	// so that you can call require('ng-models').init(mongoose) and it'd stil work
+	return module.exports;
+}
+
+function loadManifest () {
+	manifest.forEach(function(model) {
+		module.exports[model] = require('../lib/models/'+model);
+	});
+}
